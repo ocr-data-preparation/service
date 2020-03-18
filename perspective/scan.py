826c617e92db2442c708d411532db56cec870c02
rizqee <arizqee@gmail.com>
2020-03-18 19:38:02 +0700

    udah dijadiin fungsi python

--- perspective/scan.py
+++ perspective/scan.py
@@ -6,30 +6,31 @@ import argparse
 import cv2
 import imutils
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True,
-	help = "Path to the image to be scanned")
-args = vars(ap.parse_args())
+#fungsi membaca sebuah image lalu menggelapkannya untuk mendapatkan silhout garis dari image 
+# lalu membetulkan prespective image setelah silhout digunakan
+def parse_image(image):
 
-image = cv2.imread(args["image"])
-ratio = image.shape[0] / 500.0
-orig = image.copy()
-image = imutils.resize(image, height = 500)
+	image = cv2.imread(image)
+	ratio = image.shape[0] / 500.0
+	orig = image.copy()
+	image = imutils.resize(image, height = 500)
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
-edged = cv2.Canny(gray, 75, 200)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (5, 5), 0)
+	edged = cv2.Canny(gray, 75, 200)
 
-cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+	cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
 
-for c in cnts:
-	peri = cv2.arcLength(c, True)
-	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-	if len(approx) == 4:
-		screenCnt = approx
-		break
+	for c in cnts:
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+		if len(approx) == 4:
+			screenCnt = approx
+			break
+	if(screenCnt is not None):
+		warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+		#ngenulis ke file baru yang namanya transformed.jpeg
+		cv2.imwrite("transformed.jpeg",warped)
 
-warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
-cv2.imwrite("transformed.jpeg",warped)
